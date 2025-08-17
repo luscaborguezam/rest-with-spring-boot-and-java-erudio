4184a7577c543475585347d00a95e233dfe0bdac
Lucas Borguezam <85205753+luscaborguezam@users.noreply.github.com>
2025-08-17 11:18:24 -0300

    Adiciona Cors global

--- 17-TestsIntegracao/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
+++ 17-TestsIntegracao/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -2,10 +2,12 @@ package br.com.erudio.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.erudio.serialization.converter.YamlJackson2HttpMesageConverter;
@@ -16,6 +18,31 @@ import br.com.erudio.serialization.converter.YamlJackson2HttpMesageConverter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 	
+	/**
+	 * Busca as propriedades definidas no "cors.originsPatterns"
+	 */
+	@Value("${cors.originPatterns}")
+	private String corsOriginPatterns = "";
+	
+	
+	/**
+	 * Método de confiuuração para gerenciar cors globalmente
+	 * -> Refactor -> implements methods -> addCorsMappings
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		/*Lista de origens permitidas*/
+		var allowedOrigins = corsOriginPatterns.split(",");
+		
+		//Regras de mapeamento para cors
+		registry.addMapping("/**")//todas requisições vão ser filtradas por cors
+			.allowedOrigins(allowedOrigins)
+			//.allowedMethods("POST","POST", "PUT", "DELETE", "OPTIONS")
+			//     OU
+			.allowedMethods("*")
+				.allowCredentials(true);
+	}
+
 	public static final MediaType MEDIA_TYPE_APLICATION_YAML = MediaType.valueOf("application/x-yaml");
 	
 	/**
@@ -28,16 +55,6 @@ public class WebConfig implements WebMvcConfigurer{
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		/* Via Extension. http://localhost:8080/person/v1.xml DEPRECATED ON SpringBoot 2.6(ou algo assim) */
-		
-		/* VIA QUERY PARAM http://localhost:8080/person/v1?mediaType=xml */
-		/*
-		configurer.favorParameter(true)
-		.parameterName("mediaType").ignoreAcceptHeader(true)
-		.useRegisteredExtensionsOnly(false)
-		.defaultContentType(MediaType.APPLICATION_JSON)
-			.mediaType("json", MediaType.APPLICATION_JSON)
-			.mediaType("xml", MediaType.APPLICATION_XML);*/
 		
 		/* VIA HEADER PARAM http://localhost:8080/person/v1?mediaType=xml */
 		configurer.favorParameter(false)
@@ -48,6 +65,12 @@ public class WebConfig implements WebMvcConfigurer{
 			.mediaType("xml", MediaType.APPLICATION_XML)
 			.mediaType("x-yaml", MEDIA_TYPE_APLICATION_YAML);
 	}
+
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		// TODO Auto-generated method stub
+//		WebMvcConfigurer.super.addCorsMappings(registry);
+//	}
 	
 
 	
--- 17-TestsIntegracao/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 17-TestsIntegracao/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,7 @@ public class PersonController implements PersonControllerDocs {
 	private PersonServices service;
 	
 	
-	
+//	@CrossOrigin(origins = {"https://localhost", "https://lucas.teste.com"})
 	@Override
 	@GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public List<PersonVO> findAll() {
