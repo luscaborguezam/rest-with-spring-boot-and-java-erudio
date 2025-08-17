c56745b2ce391be468def1b08a48b091eed104f1
Lucas Borguezam <85205753+luscaborguezam@users.noreply.github.com>
2025-08-17 12:37:05 -0300

    configura testes

---
+++ 17-TestsIntegracao/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/config/TestConfigs.java
@@ -0,0 +1,13 @@
+package br.com.erudio.config;
+
+public interface TestConfigs {
+    int SERVER_PORT = 8888;
+    
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+    
+    String ORIGIN_ACCEPT = "https://lucas.teste.com";
+    String ORIGIN_NOT_ACCEPT = "https://outros.teste.com";
+//    String ORIGIN_LOCAL = "http://localhost:8080";-----------------String ORIGIN_LOCAL = "http://localhost:8080";-----------------
+    
+}
