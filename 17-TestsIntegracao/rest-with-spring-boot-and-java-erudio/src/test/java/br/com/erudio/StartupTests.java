d2ed6f635d642871decf1979481f963bf8662198
Lucas Borguezam <85205753+luscaborguezam@users.noreply.github.com>
2025-08-21 19:36:28 -0300

    Configura teste de integração com docker
    Não funciona por algum motivo de permissão. o programa não consegue acessar o container do mysql

--- 17-TestsIntegracao/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/StartupTests.java
+++ 17-TestsIntegracao/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/StartupTests.java
@@ -2,8 +2,10 @@ package br.com.erudio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 class StartupTests {
 
 	@Test
---
+++ 17-TestsIntegracao/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/testcontainers/AbstractIntegrationTestBakup.java
@@ -0,0 +1,64 @@
+//package br.com.erudio.integrationtests.testcontainers;
+//
+//import java.util.Map;
+//import java.util.stream.Stream;
+//
+//import org.springframework.context.ApplicationContextInitializer;
+//import org.springframework.context.ConfigurableApplicationContext;
+//import org.springframework.core.env.ConfigurableEnvironment;
+//import org.springframework.core.env.MapPropertySource;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.testcontainers.containers.MySQLContainer;
+//import org.testcontainers.lifecycle.Startables;
+//
+//
+//@ContextConfiguration(initializers = AbstractIntegrationTestBakup.Initializer.class)
+//public class AbstractIntegrationTestBakup {
+//
+//	/**
+//	 *  Initializer.class é a classe respopnssável por criar um container do mysql dinamicamente através do testcontainers
+//	 */
+//	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+//
+//		//CONTAINER DO MySQL 
+//		//Especificando versão da image do docker hub
+//		static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:9.3.0");
+//
+//		//CRIA O CONTAINER DO MYSQL
+//		private static void startContainers() {
+//			//Pega os parametros definidos na variável mysql e itera sobre eles
+//			Startables.deepStart(Stream.of(mysql)).join();
+//		}
+//		
+//		//DEFINIR AS CONFIGURAÇÕES DE CONEXAO COM O BANCO
+//		//PARTE MAIS SENSÍVEL POIS O BANCO É CRIADO DINAMICAMENTE, A CADA URL O USUÁRIO E SENHA VÃO MUDAR.
+//		//
+//		private static Map<String, Object> createConnectionConfiguration() {
+//			//USAR AS CONFIGURAÇÕES DO APLICATION PROPERTIES/YAML PRINCIPAL DO PROJETO E ADAPTALAS
+//			return Map.of(
+//					"spring.datasource.url", mysql.getJdbcUrl(),
+//					"spring.datasource.username", mysql.getUsername(),
+//					"spring.datasource.password", mysql.getPassword()
+//			);
+//		}
+//		
+//		@Override
+//		public void initialize(ConfigurableApplicationContext applicationContext) {
+//			startContainers();
+//			//OBTEM AS VARIÁVEIS DE AMBIENTE (anotations, classe de configurações, aplication.yaml/properties)
+//			ConfigurableEnvironment environment = applicationContext.getEnvironment();
+//			
+//			//CONFIGURAÇÃO DO TESTCONTAINERS, 
+//			MapPropertySource testcontainers = new MapPropertySource("testcontainers", 
+//					createConnectionConfiguration());
+//			
+//			//ADICIONA AS CONFIGURAÇÕES DEFINIDAS NO testcontainers ANTES DE QUALQUER OUTRA NO AMBIENTE DO SPRING
+//			environment.getPropertySources().addFirst(testcontainers);			
+//			
+//			
+//		}
+//
+//
+//
+//	}
+//}
